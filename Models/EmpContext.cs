71c4772c699eef1b12b935e33fb7368b5b53aad7
salman1256 <salman1256@yahoo.com>
2020-04-02 12:40:57 +0530

    After Adding Ef packages and Context Class

---
+++ Models/EmpContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DbEnabledApp.Models
+{
+    public class EmpContext : DbContext
+    {
+        public EmpContext( DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Emp> Emps { get; set; }
+    }
+}
