72eb29fe6bc3249bed048fb4b7b857ff64ac2a6f
salman1256 <salman1256@yahoo.com>
2020-04-02 12:28:12 +0530

    First Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DbEnabledApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
