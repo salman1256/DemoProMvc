b6c9dafd0bcf54807c81b10822bd70a7f209d18f
salman1256 <salman1256@yahoo.com>
2020-04-02 12:31:52 +0530

    Emp Model Added

---
+++ Models/Emp.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DbEnabledApp.Models
+{  [Table("Emp")]
+    public class Emp
+    {    [Key]
+        public int MyProperty { get; set; }
+        public string  Name { get; set; }
+        public string  Desig { get; set; }
+    }
+}
