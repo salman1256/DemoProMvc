99115b61c83799f5779a31eed8a595d8cf2a839d
salman1256 <salman1256@yahoo.com>
2020-04-02 13:01:25 +0530

    adding migrations

---
+++ Migrations/20200402072946_our-mig.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DbEnabledApp.Migrations
+{
+    public partial class ourmig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Emp",
+                columns: table => new
+                {
+                    MyProperty = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Desig = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Emp", x => x.MyProperty);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Emp");
+        }
+    }
+}
