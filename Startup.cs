96b789013c78f5c2128ebbd2436cf44ba5128993
salman1256 <salman1256@yahoo.com>
2020-04-02 12:59:12 +0530

    adding Context Class and connection

--- Startup.cs
+++ Startup.cs
@@ -1,10 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
+using DbEnabledApp.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +22,9 @@ namespace DbEnabledApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<EmpContext>(options =>
+            options.UseSqlServer
+            (Configuration.GetConnectionString("OurConStr")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
